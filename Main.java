275dd455599c2960ea5f2191f609ea1801c06f1b
Ruslan <sabackas@yandex.ru>
2023-08-09 14:27:10 +0400

    AllFileHomeWoer

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import controller.ControllerToy;
+import model.FileOperationToy;
+import model.MapperToy;
+import model.RepositoryToy;
+import view.ViewToy;
+
+public class Main {
+    public static Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("logging.properties"));
+        logger.info("Main run");
+        FileOperationToy fileOperationToys = new FileOperationToy("Java/Toys.txt", logger);
+        FileOperationToy fileOperationPrizeToys = new FileOperationToy("Java/PrizeToys.txt", logger);
+        FileOperationToy fileOperationGivenOutPrizeToys = new FileOperationToy("Java/GivenOutPrizeToys.txt", logger);
+        RepositoryToy repositoryToys = new RepositoryToy(fileOperationToys, new MapperToy());
+        RepositoryToy repositoryPrizeToys = new RepositoryToy(fileOperationPrizeToys, new MapperToy());
+        RepositoryToy repositoryGivenOutPrizeToys = new RepositoryToy(fileOperationGivenOutPrizeToys, new MapperToy());
+        ControllerToy controllerToys = new ControllerToy(repositoryToys, logger);
+        ControllerToy controllerPrizeToys = new ControllerToy(repositoryPrizeToys, logger);
+        ControllerToy controllerGivenOutPrizeToy = new ControllerToy(repositoryGivenOutPrizeToys, logger);
+        ViewToy view = new ViewToy(controllerToys, controllerPrizeToys, controllerGivenOutPrizeToy, logger);
+        view.run();
+    }
+}
